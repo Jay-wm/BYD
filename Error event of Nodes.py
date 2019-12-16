ef62af26f6e16f34b0fc8ec9b2ea5cb1cb23b35a
Jay-wm <55948899+Jay-wm@users.noreply.github.com>
2019-12-17 00:10:15 +0800

    over

--- Error event of Nodes.py
+++ Error event of Nodes.py
@@ -1,20 +1,30 @@
-import requests
 import lxml.html
 import csv
+import os
 
-html_code = ''
-selector = lxml.html.fromstring(hml_code)
-EventEmcyCode = sellector.xpath('//*[@id="TableContentBkg"]/td[2]')
-Group = sellector.xpath('//*[@id="EventGroup"]')
-Event = sellector.xpath('//*[@id="EventName"]')
-Description = sellector.xpath('//*[@id="EventDescription"]')
-Event_ID = sellector.xpath('//*[@id="event-100u"]')
+html_path = os.path.expanduser('69GM2046C00x04-node6.htm')
 
+with open('69GM2046C00x04-node6.htm', encoding = 'utf-8') as f:
+    html_code = f.read()
+# html = requests.get('file:///D:/69GM2046C00x04-node6.htm').content.decode()
+selector = lxml.html.fromstring(html_code)
+# EventEmcyCode = selector.xpath('//tbody[@id="TableOfEvents"]/tr[@id="TableContentBkg"]/td[2]/text()')
+Group = selector.xpath('//*[@id="EventGroup"]/text()')
+Event = selector.xpath('//*[@id="EventName"]/text()')
+# Description_all = selector.xpath('//*[@id="EventDescription"]')
+# for a in Description_all:
+#     Description = a.xpath('string(.)')
+Event_ID = selector.xpath('//tbody[@id="TableOfEvents"]/tr[@id="TableContentBkg"]/td[6]/text()')
+print(len(Group))
 
-data = [{}]
-titles = ['EventEmcyCode', 'Group', 'Event', 'Description', 'Event_ID']
-with open('C80D-Error_Event.csv', encoding = 'utf-8') as f:
-    writer = csv.DictWriter(f, filename = titles)
+data = []
+for i in range(131):
+    dict = {'Group': Group[i], 'Event': Event[i], 'Event_ID': Event_ID[i]}
+    data.append(dict)
+
+# titles = ['EventEmcyCode', 'Group', 'Event', 'Description', 'Event_ID']
+titles = ['Group', 'Event', 'Event_ID']
+with open('C80D-Error_Event.csv', 'w', encoding = 'UTF-8') as f:
+    writer = csv.DictWriter(f, fieldnames = titles)
     writer.writeheader()
     writer.writerows(data)
-    
