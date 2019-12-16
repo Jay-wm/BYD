5d66219b0d24d0999204ab70b7a01e4a5e3f2eec
Jay-wm <55948899+Jay-wm@users.noreply.github.com>
2019-12-16 16:25:35 +0800

    Update Error event of Nodes.py

--- Error event of Nodes.py
+++ Error event of Nodes.py
@@ -1,10 +1,20 @@
 import requests
 import lxml.html
+import csv
 
-html_code= ''
+html_code = ''
 selector = lxml.html.fromstring(hml_code)
 EventEmcyCode = sellector.xpath('//*[@id="TableContentBkg"]/td[2]')
 Group = sellector.xpath('//*[@id="EventGroup"]')
 Event = sellector.xpath('//*[@id="EventName"]')
 Description = sellector.xpath('//*[@id="EventDescription"]')
 Event_ID = sellector.xpath('//*[@id="event-100u"]')
+
+
+data = [{}]
+titles = ['EventEmcyCode', 'Group', 'Event', 'Description', 'Event_ID']
+with open('C80D-Error_Event.csv', encoding = 'utf-8') as f:
+    writer = csv.DictWriter(f, filename = titles)
+    writer.writeheader()
+    writer.write(data)
+    
